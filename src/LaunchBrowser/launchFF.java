660c9b4c6aee612f0ea3c752635ebe34307c01e4
ASawai <sawai.abhishek@gmail.com>
2018-11-28 00:30:54 +0530

    launching ff

---
+++ src/LaunchBrowser/launchFF.java
@@ -0,0 +1,10 @@
+package LaunchBrowser;
+
+public class launchFF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("we are launching ff");
+	}
+
+}
